32056b261702ef9b60cef85dcc9800ee52d7110c
장경석 <skhy.X0146220@partner.sk.com>
2025-02-28 14:57:27 +0900

    메인 화면 상단에 메뉴 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import OneGridPage from 'pages/OneGridPage';
 import TwoGridPage from 'pages/TwoGridPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<OneGridPage />} />
-        <Route path="/one" element={<OneGridPage />} />
-        <Route path="/two" element={<TwoGridPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='w-full h-full px-0 py-0'>
+      <BrowserRouter>
+        <div className='flex flex-row justify-between h-10 bg-gray-200 items-center'>
+          <div>
+            <h1>Hello World</h1>
+          </div>
+          <div>
+            <Link to="/"  className='px-2 cursor-pointer'>Home</Link>
+            <Link to="/one" className='px-2 cursor-pointer'>One</Link>
+            <Link to="/two" className='px-2 cursor-pointer'>Two</Link>
+          </div>
+        </div>
+        <div className='flex flex-row justify-between'>
+          <Routes>
+            <Route path="/" element={<OneGridPage />} />
+            <Route path="/one" element={<OneGridPage />} />
+            <Route path="/two" element={<TwoGridPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
