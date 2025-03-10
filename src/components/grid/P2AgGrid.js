7a117c1cb3d1c92ad79d6e888d5667fed4fca3ba
장경석 <skhy.X0146220@partner.sk.com>
2025-03-11 08:29:33 +0900

    useEffect 적용

--- src/components/grid/P2AgGrid.js
+++ src/components/grid/P2AgGrid.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useCallback, useImperativeHandle, forwardRef } from "react";
+import React, { useMemo, useState, useCallback, useImperativeHandle, forwardRef, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 import { themeQuartz } from "ag-grid-community";
 import { 
@@ -29,6 +29,7 @@ function P2AgGrid(props, ref) {
   });
   const [rowData] = useState(props.rowData || []);
   const [gridApi, setGridApi] = useState();
+  const [defaultHeaderHeight, setDefaultHeaderHeight] = useState(props.headerHeight || 35);
 
   useImperativeHandle(ref, () => ({
     api: gridApi,
@@ -39,145 +40,148 @@ function P2AgGrid(props, ref) {
   const showCheckedColumn = props.showCheckedColumn;
   const showStatusColumn = props.showStatusColumn;
 
-  if (showStatusColumn) {
-    const columns = columnDefs.filter((c) => c.field === statusField);
+  useEffect(() => {
 
-    if (!columns.length) {
-      let statusColumn = {
-        headerName: "상태",
-        field: statusField,
-        width: 70,
-        editable: false,
-        sortable: false,
-        filter: false,
-        cellClass: "text-center",
-      };
+    if (showStatusColumn) {
+      const columns = columnDefs.filter((c) => c.field === statusField);
 
-      setColumnDefs((prev) => [statusColumn, ...prev]);
-    }
-  }
-
-  if (showCheckedColumn) {
-    const columns = columnDefs.filter((c) => c.key === checkedField);
-    if (!columns.length) {
-      let checkColumn = {
-        key: checkedField,
-        headerName: " ",
-        width: 50,
-        sortable: false,
-        filter: false,
-        editable: true,
-        cellRenderer: CommonCheckedRenderer,
-        cellEditor: CommonCheckedEditor,
-        headerComponent: CommonHeaderCheckedComponet,
-      };
-
-      setColumnDefs((prev) => [checkColumn, ...prev]);
-    }
-  }
-
-  if (showRowNumColumn) {
-    const columns = columnDefs.filter((c) => c.field === rowNumField);
-
-    if (!columns.length) {
-      let rowNumColumn = {
-        headerName: "No",
-        field: rowNumField,
-        width: 60,
-        sortable: false,
-        filter: false,
-        editable: false,
-        cellClass: "text-center",
-        valueFormatter: (params) => {
-          return `${parseInt(params.node.childIndex) + 1}`;
-        },
-      };
-
-      setColumnDefs((prev) => [rowNumColumn, ...prev]);
-    }
-  }
-
-  let defaultHeaderHeight = 35;
-  let required = {
-    'error-cell': (params) => !params.value
-  };
-
-  columnDefs.forEach((c) => {
-    if (c.headerName.includes("\r\n")) {
-      defaultHeaderHeight = undefined;
-    }
-    c.headerClass = "justify-center";
-
-    if (c.align) {
-      switch (c.align) {
-        case "center":
-          c.cellClass = `${c.cellClass} text-center`;
-          break;
-        case "right":
-          c.cellClass = `${c.cellClass} text-right`;
-          break;
-        default:
-          break;
-      }
-    }
-    if (c.cellDataType === "checkbox") {
-      c.cellRenderer = P2CheckboxCellRenderer;
-      c.cellEditor = P2CheckboxCellEditor;
-      c.suppressKeyboardEvent = (params) => !!params.colDef.editable && params.event.key === ' ';
-      c.cellClass = "text-center";
-    }
-    else if (c.cellDataType === "combo") {
-      c.cellEditor = P2ComboboxCellEditor;
-      if (!c.cellEditorParams) {
-        c.cellEditorParams = {
-          valueField: "cd",
-          displayField: "cdNm",
-          values: [],
+      if (!columns.length) {
+        let statusColumn = {
+          headerName: "상태",
+          field: statusField,
+          width: 70,
+          editable: false,
+          sortable: false,
+          filter: false,
+          cellClass: "text-center",
         };
+
+        setColumnDefs((prev) => [statusColumn, ...prev]);
       }
-      c.valueFormatter = function (params) {
-        const editorParams = params.colDef.cellEditorParams;
-        const matchDatas = editorParams.values.filter(
-          (v) => v[editorParams.valueField || "cd"] === params.value
-        );
-        if (matchDatas.length) {
-          return matchDatas[0][editorParams.displayField || "cdNm"];
+    }
+
+    if (showCheckedColumn) {
+      const columns = columnDefs.filter((c) => c.key === checkedField);
+      if (!columns.length) {
+        let checkColumn = {
+          key: checkedField,
+          headerName: " ",
+          width: 50,
+          sortable: false,
+          filter: false,
+          editable: true,
+          cellRenderer: CommonCheckedRenderer,
+          cellEditor: CommonCheckedEditor,
+          headerComponent: CommonHeaderCheckedComponet,
+        };
+
+        setColumnDefs((prev) => [checkColumn, ...prev]);
+      }
+    }
+
+    if (showRowNumColumn) {
+      const columns = columnDefs.filter((c) => c.field === rowNumField);
+
+      if (!columns.length) {
+        let rowNumColumn = {
+          headerName: "No.",
+          field: rowNumField,
+          width: 60,
+          sortable: false,
+          filter: false,
+          editable: false,
+          cellClass: "text-center",
+          valueFormatter: (params) => {
+            return `${parseInt(params.node.childIndex) + 1}`;
+          },
+        };
+
+        setColumnDefs((prev) => [rowNumColumn, ...prev]);
+      }
+    }
+
+    let required = {
+      'error-cell': (params) => !params.value
+    };
+
+    columnDefs.forEach((c) => {
+      if (c.headerName.includes("\r\n")) {
+        setDefaultHeaderHeight(undefined);
+      }
+      c.headerClass = "justify-center";
+
+      if (c.align) {
+        switch (c.align) {
+          case "center":
+            c.cellClass = `${c.cellClass} text-center`;
+            break;
+          case "right":
+            c.cellClass = `${c.cellClass} text-right`;
+            break;
+          default:
+            break;
         }
-      };
-    }
-    else if (c.cellDataType === "ajaxCombo") {
-      c.cellEditor = P2AjaxComboboxCellEditor;
-      if (!c.cellEditorParams) {
-        c.cellEditorParams = {
-          valueField: "cd",
-          displayField: "cdNm",
-          values: [],
-        };
       }
-      c.valueFormatter = function (params) {
-        const editorParams = params.colDef.cellEditorParams;
-        if (editorParams.values) {
+      if (c.cellDataType === "checkbox") {
+        c.cellRenderer = P2CheckboxCellRenderer;
+        c.cellEditor = P2CheckboxCellEditor;
+        c.suppressKeyboardEvent = (params) => !!params.colDef.editable && params.event.key === ' ';
+        c.cellClass = "text-center";
+      }
+      else if (c.cellDataType === "combo") {
+        c.cellEditor = P2ComboboxCellEditor;
+        if (!c.cellEditorParams) {
+          c.cellEditorParams = {
+            valueField: "cd",
+            displayField: "cdNm",
+            values: [],
+          };
+        }
+        c.valueFormatter = function (params) {
+          const editorParams = params.colDef.cellEditorParams;
           const matchDatas = editorParams.values.filter(
             (v) => v[editorParams.valueField || "cd"] === params.value
           );
           if (matchDatas.length) {
             return matchDatas[0][editorParams.displayField || "cdNm"];
           }
-        }
-      };
-    }
-    else if (c.cellDataType === "number") {
-      c.cellClass = c.cellClass || "text-right";
-    }
-
-    if (c.required === true) {
-      if (c.headerName) {
-        c.headerName = "* " + c.headerName;
-        c.cellClassRules = required;
-        c.tooltipValueGetter = (params) => Utils.isEmpty(params.value) ? "필수입력" : "";
+        };
       }
-    }
-  });
+      else if (c.cellDataType === "ajaxCombo") {
+        c.cellEditor = P2AjaxComboboxCellEditor;
+        if (!c.cellEditorParams) {
+          c.cellEditorParams = {
+            valueField: "cd",
+            displayField: "cdNm",
+            values: [],
+          };
+        }
+        c.valueFormatter = function (params) {
+          const editorParams = params.colDef.cellEditorParams;
+          if (editorParams.values) {
+            const matchDatas = editorParams.values.filter(
+              (v) => v[editorParams.valueField || "cd"] === params.value
+            );
+            if (matchDatas.length) {
+              return matchDatas[0][editorParams.displayField || "cdNm"];
+            }
+          }
+        };
+      }
+      else if (c.cellDataType === "number") {
+        c.cellClass = c.cellClass || "text-right";
+      }
+
+      if (c.required === true) {
+        if (c.headerName) {
+          c.headerName = "* " + c.headerName;
+          c.cellClassRules = required;
+          c.tooltipValueGetter = (params) => Utils.isEmpty(params.value) ? "필수입력" : "";
+        }
+      }
+      
+    });
+  }, []);
 
   const rowSelection = useMemo(() => { 
     return {
@@ -211,22 +215,22 @@ function P2AgGrid(props, ref) {
     };
   }, []);
 
-    const onGridReady = useCallback((params) => {
-      if (props.api) {
-        props.api(params.api);
-      }
-      if (ref) {
-        setGridApi(params.api);
-      }
-      
-      Object.keys(P2AgGridModule.apiFunctions).forEach((key) => {
-        params.api[key] = P2AgGridModule.apiFunctions[key].bind(this, params.api);
-      });
-      
-      if (props.onGridReady) {
-        props.onGridReady(params);
-      }
-    }, []);
+  const onGridReady = useCallback((params) => {
+    if (props.api) {
+      props.api(params.api);
+    }
+    if (ref) {
+      setGridApi(params.api);
+    }
+    
+    Object.keys(P2AgGridModule.apiFunctions).forEach((key) => {
+      params.api[key] = P2AgGridModule.apiFunctions[key].bind(this, params.api);
+    });
+    
+    if (props.onGridReady) {
+      props.onGridReady(params);
+    }
+  }, []);
 
   const onCellValueChanged = useCallback((params) => {
     if (props.onCellValueChanged) {
@@ -251,7 +255,7 @@ function P2AgGrid(props, ref) {
       defaultColDef={defaultColDef}
       rowData={rowData}
       rowHeight={props.rowHeight || 35}
-      headerHeight={props.headerHeight || defaultHeaderHeight}
+      headerHeight={defaultHeaderHeight}
       rowSelection={props.rowSelection || rowSelection}
       singleClickEdit={props.singleClickEdit || true}
       tooltipShowDelay={props.tooltipShowDelay || 500}
