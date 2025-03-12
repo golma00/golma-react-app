5eae777e7b0bd6d753028d1dd6698e38b83f66c5
장경석 <skhy.X0146220@partner.sk.com>
2025-03-12 13:57:22 +0900

    primary color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          '50': '#edf9ff',
+          '100': '#d6efff',
+          '200': '#b6e5ff',
+          '300': '#84d7ff',
+          '400': '#4abfff',
+          '500': '#219eff',
+          '600': '#097fff',
+          '700': '#0366f2',
+          '800': '#0a51c3',
+          '900': '#0e4491',
+          '950': '#0e2c5d',
+        }
+      },
       backgroundColor: {
         "area-bg-color": "var(--area-bg-color)",
       },
       borderColor: {
         "area-border-color": "var(--area-border-color)",
       },
+      
     },
   },
   plugins: [],
