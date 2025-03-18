6479c919813a5aabb902e9aad6a2ca40c270973f
장경석 <skhy.X0146220@partner.sk.com>
2025-03-18 11:04:41 +0900

    menuProp 전달 받아서 권한 체크 하도록 처리

--- src/components/layout/P2GridButtonBar.js
+++ src/components/layout/P2GridButtonBar.js
@@ -9,18 +9,18 @@ function P2GridButtonBar(props) {
         {props.title && (
           <span className="text-base self-end text-ellipsis whitespace-nowrap">{props.title}</span>
         )}
-        {props.count > 1 && (
+        {props.count > 0 && (
           <span className="text-base self-end text-ellipsis whitespace-nowrap">({props.count.toLocaleString()} 건)</span>
         )}
       </div>
       <div className="flex flex-row w-full h-8 gap-1 justify-end">
-        {props.onAddRow && (
+        {props.onAddRow && props.menuProps&& props.menuProps.saveUseYn === "Y" && (
           <button className="grid-btn" onClick={props.onAddRow}>
             <FontAwesomeIcon icon="fa-plus" className="pr-1" size="sm"/>
             <span>행추가</span>
           </button>
         )}
-        {props.onDeleteRow && (
+        {props.onDeleteRow && props.menuProps && props.menuProps.saveUseYn === "Y" && (
           <button className="grid-btn" onClick={props.onDeleteRow}>
             <FontAwesomeIcon icon="fa-minus" className="pr-1" size="sm"/>
             <span>행삭제</span>
@@ -30,7 +30,12 @@ function P2GridButtonBar(props) {
           Children.map(props.children, (child, index) => {
             return (
               <React.Fragment key={index}>
-                {child}
+                {
+                  child.props.auth && props.menuProps && props.menuProps[child.props.auth] === "Y" && child
+                }
+                {
+                  !child.props.auth && child
+                }
               </React.Fragment>
             );
           })
