a6e24c7515949e9a4434c96a64d0fa72192bedde
장경석 <skhy.X0146220@partner.sk.com>
2025-03-24 09:56:10 +0900

    훅 명칭 변경 필요

--- src/hooks/useCommonCode.js
+++ src/hooks/useCommonCode.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export const useCommonCode = () => {
+export const useCommonCode = () => { // useCodeData
   /**
    * 공통코드 데이터 조회
    * @param {Object} params 
    * @returns {} 공통코드 데이터 배열
    */
-  async function getCodeDatas(params) {
+  async function getCodeDatas(params) { // getCommonCodeDatas
     const res = await axios.post("/api/v1/code/getCodeDatas", {
       commonCodeParams: params,
     });
